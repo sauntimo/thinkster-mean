8052d1cdcb97e6c563b1f747ac862f0375bc2098
Tim Saunders <tim@sauntimo.org>
2016-03-18 01:14:33 +0000

    Initial commit of thinkster.io mean tutorial

---
+++ app.js
@@ -0,0 +1,66 @@
+
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider){
+
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: '/home.html',
+				controller: 'MainCtrl'
+		    })
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			});
+
+			$urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts' [function(){
+	var o = {
+		posts: [
+			{title: 'post 1', upvotes: 5},
+			{title: 'post 2', upvotes: 2}
+		]
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [ '$scope', 'posts', function($scope, posts){
+
+	$scope.posts = posts.posts;
+
+	$scope.addPost = function(){
+		if( !$scope.title || $scope.title === '' ){
+			return;
+		}
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		});
+		
+		$scope.title = '';
+		$scope.link = '';
+
+	};
+
+	$scope.incrementUpvotes = function(post){
+		post.upvotes++;
+	}
+
+}]);
+
+app.controller('PostsCtrl',[
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateparams, posts){
+
+}]);
+
